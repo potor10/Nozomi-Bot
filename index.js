18661fd60c6b69db28768a98263594fed76f7d98
Potor <liupotor4@gmail.com>
2021-02-16 05:05:18 -0800

    new

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ const returnOCR = async message => {
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
-    message.attachments.forEach(function(attachment) {
+    message.attachments.forEach(async function(attachment) {
         const { data: { text } } = await worker.recognize(attachment.url);
         await message.reply(`The text in the image is: ${text}`);
         console.log(text);
