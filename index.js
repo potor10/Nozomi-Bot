24bc65f0c6393c158147cdceb5d659baab54c719
Potor <liupotor4@gmail.com>
2021-02-16 11:59:23 -0800

    new

--- index.js
+++ index.js
@@ -243,8 +243,8 @@ const addXp = async message => {
     let currentTime = Date.now();
     let profile = await retrieveStats(message.author.id);
 
-    console.log(profile);
-    console.log(profile.lastMessage);
+    console.log(`Test From here: ${profile}`);
+    console.log(`Last message: ${profile.lastMessage}`);
     if (currentTime - profile.lastMessage > 3000) { //missing 00
         let newXP = profile.exp + Math.floor(Math.random() * 5);
         console.log(`LOG: ${newXP - profile.exp} XP has been granted to ${message.author.username} (${message.author.id})`);
