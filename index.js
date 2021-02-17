fa7e065aa1f24d71ed0cf022f0cfd929fd9eaedb
Potor <liupotor4@gmail.com>
2021-02-16 22:37:16 -0800

    new

--- index.js
+++ index.js
@@ -473,8 +473,8 @@ const returnOCR = async message => {
             
             let date;
             for (i = 0; i < values[1].length - 6; i++) {
-                date = Date.parse(`${values[1].substr(i, 6)} ${new Date().getUTCFullYear()}`);
-                if(!isNaN(date)){
+                if(!isNaN(Date.parse(values[1].substr(i, 6)))){
+                    date = Date.parse(`${values[1].substr(i, 6)} ${new Date().getUTCFullYear()}`);
                     console.log(`LOG: Date Parsed, Found ${date} from ${values[1].substr(i, 6)} ${new Date().getUTCFullYear()}`);
                     break;
                 }
