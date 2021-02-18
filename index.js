84afcefa13eec590d02d146ab41185f82187d826
Potor <liupotor4@gmail.com>
2021-02-18 13:10:48 -0800

    new

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const parseFirstArgAsInt = (args, defaultValue) => {
     if (!Array.isArray(args)) return defaultValue;
     if (args.length) {
         let parseAmt = parseInt(args.shift().toLowerCase(), 10);
-        if (!isNaN(parseAmt) && parseAmt > 0) return parseAmt;
+        if (!isNaN(parseAmt) && parseAmt >= 0) return parseAmt;
     } else return defaultValue;
 };
 
