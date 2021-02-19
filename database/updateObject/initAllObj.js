0fa9152d298e85d3e5210e71f74202fe2a62c6fa
Potor <liupotor4@gmail.com>
2021-02-19 06:49:14 -0800

    split up files

--- database/updateObject/initAllObj.js
+++ database/updateObject/initAllObj.js
@@ -8,6 +8,10 @@ module.exports = async (data) => {
     let initCbid = require('./initCbidObj');
     let cbid = await initCbid();
 
+    console.log(userData);
+    console.log(collectionData);
+    console.log(cbid);
+
     data.userData = userData;
     data.collectionData = collectionData;
     data.currentClanBattleId = cbid;
