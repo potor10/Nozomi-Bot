75606cba7986fc7dbc7c881c460845c6070e9778
Potor <liupotor4@gmail.com>
2021-02-25 02:39:25 -0800

    ok

--- commands/core/help.js
+++ commands/core/help.js
@@ -5,6 +5,48 @@ module.exports = {
     utilisation: '{prefix}help',
 
     execute(client, message, args) {
+        if (!args[0]) {
+            const infos = message.client.commands.filter(x => x.category == 'Infos').map((x) => '`' + x.name + '`').join(', ');
+            const music = message.client.commands.filter(x => x.category == 'Music').map((x) => '`' + x.name + '`').join(', ');
+
+            message.channel.send({
+                embed: {
+                    color: 'ORANGE',
+                    author: { name: 'Help pannel' },
+                    footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
+                    fields: [
+                        { name: 'Bot', value: infos },
+                        { name: 'Music', value: music },
+                        { name: 'Filters', value: client.filters.map((x) => '`' + x + '`').join(', ') },
+                    ],
+                    timestamp: new Date(),
+                    description: `To use filters, ${client.config.discord.prefix}filter (the filter). Example : ${client.config.discord.prefix}filter 8D.`,
+                },
+            });
+        } else {
+            const command = message.client.commands.get(args.join(" ").toLowerCase()) || message.client.commands.find(x => x.aliases && x.aliases.includes(args.join(" ").toLowerCase()));
+
+            if (!command) return message.channel.send(`${client.emotes.error} - I did not find this command !`);
+
+            message.channel.send({
+                embed: {
+                    color: 'ORANGE',
+                    author: { name: 'Help pannel' },
+                    footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
+                    fields: [
+                        { name: 'Name', value: command.name, inline: true },
+                        { name: 'Category', value: command.category, inline: true },
+                        { name: 'Aliase(s)', value: command.aliases.length < 1 ? 'None' : command.aliases.join(', '), inline: true },
+                        { name: 'Utilisation', value: command.utilisation.replace('{prefix}', client.config.discord.prefix), inline: true },
+                    ],
+                    timestamp: new Date(),
+                    description: 'Find information on the command provided.\nMandatory arguments `[]`, optional arguments `<>`.',
+                }
+            });
+        };
+        
+        /*
+
         message.author.send(
             `*I'll be counting on you, so let's work together until I can become a top idol, okay?\n` + 
             `Ahaha, from now on, I'll be in your care!* \n\n\n` + 
@@ -74,5 +116,6 @@ module.exports = {
         message.author.send(ex2);
         message.author.send(ex3);
         message.author.send(ex4);
+         */
     },
 };
