61f3e3d596e4e22fe0ba0cce3427dffc87e78093
Potor <liupotor4@gmail.com>
2021-02-24 22:55:57 -0800

    ok

--- database/updateDatabase/updateAll.js
+++ database/updateDatabase/updateAll.js
@@ -1,10 +1,11 @@
 module.exports = async (client) => {
     let updateCBID = require('./updateCbid');
     await updateCBID(client.currentClanBattleId);
-    
+
     for(let id in client.userData) {
         if (client.userData.hasOwnProperty(id)) {
-            await updateStatsDB(id, client.userData[id].level, client.userData[id].exp, client.userData[id].lastmessage, 
+            let updateStats = require('./updateStats');
+            await updateStats(id, client.userData[id].level, client.userData[id].exp, client.userData[id].lastmessage, 
                 client.userData[id].jewels, client.userData[id].amulets);
         }
     } 
