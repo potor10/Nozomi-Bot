9f79433b0df115ea3bf5bff20cf51881069ef90d
Potor <liupotor4@gmail.com>
2021-02-28 14:21:41 -0800

    new

--- helper/gacha/updateGacha.js
+++ helper/gacha/updateGacha.js
@@ -6,9 +6,9 @@ module.exports = async (client) => {
     await webScrapeData(client, arcticurl);
     
     const rwikiurls = [];
-    urls.push('https://rwiki.jp/priconne_redive/%E3%82%AD%E3%83%A3%E3%83%A9/%E2%98%85');
-    urls.push('https://rwiki.jp/priconne_redive/%E3%82%AD%E3%83%A3%E3%83%A9/%E2%98%85%E2%98%85');
-    urls.push('https://rwiki.jp/priconne_redive/%E3%82%AD%E3%83%A3%E3%83%A9/%E2%98%85%E2%98%85%E2%98%85');
+    //rwikiurls.push('https://rwiki.jp/priconne_redive/%E3%82%AD%E3%83%A3%E3%83%A9/%E2%98%85');
+    //rwikiurls.push('https://rwiki.jp/priconne_redive/%E3%82%AD%E3%83%A3%E3%83%A9/%E2%98%85%E2%98%85');
+    //rwikiurls.push('https://rwiki.jp/priconne_redive/%E3%82%AD%E3%83%A3%E3%83%A9/%E2%98%85%E2%98%85%E2%98%85');
 
     const findTableRwiki = '.table > tbody > tr > td > a';
     const findImgRwiki = '.ie5 > table > tbody > tr > td';
