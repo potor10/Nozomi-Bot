0696bb23183f44f0d8a610f44a97aa6cee928d83
Potor <liupotor4@gmail.com>
2021-02-28 15:40:06 -0800

    new

--- helper/gacha/webScrapeArctic.js
+++ helper/gacha/webScrapeArctic.js
@@ -12,7 +12,7 @@ module.exports = async (client, url) => {
         let links = $(findTable);
 
         for (let i = 0; i < links.length; i++) {
-            console.log(links[i].attribs);
+            console.log(links[i].attribs.href);
             //console.log(links[i].attr('href'));
             //console.log(links[i].attribs.href);
             //let getGachaDataArctic = require('./getGachaDataArctic');
