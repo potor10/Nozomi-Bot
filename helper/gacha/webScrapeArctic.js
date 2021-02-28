9ede5d7995666f09f29023a21380f8dc7b1e3c4b
Potor <liupotor4@gmail.com>
2021-02-28 15:18:31 -0800

    new

--- helper/gacha/webScrapeArctic.js
+++ helper/gacha/webScrapeArctic.js
@@ -6,10 +6,10 @@ module.exports = async (client, url) => {
         const response = await got(url);
         let $ = cheerio.load(response.body);
         console.log(`LOG: Finding Arctic Passerine Data From Units From: ${url}`);
-        console.log(response.body);
-        const findTable = '#chartable > tbody > tr > .bcell th > a';
+        const findTable = '#chartable > tbody > tr > .bcell > a';
 
         let links = $(findTable);
+        console.log(links);
 
         for (let i = 0; i < links.length; i++) {
             console.log(links.html());
