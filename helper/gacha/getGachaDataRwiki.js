ae2c748cef59607074bed28b741e8f03e457808d
Potor <liupotor4@gmail.com>
2021-02-28 19:35:22 -0800

    new

--- helper/gacha/getGachaDataRwiki.js
+++ helper/gacha/getGachaDataRwiki.js
@@ -2,13 +2,13 @@ module.exports = async (client, href, charname, starlevel) => {
     const cheerio = require('cheerio');
     const got = require("got");
     
-    const findImg = '.ie5 > table > tbody > tr > td > .style_td img';
+    const findImg = '.ie5 > table > tbody > tr > .style_td img';
 
     try {
 		const response = await got(href);
         let $ = cheerio.load(response.body);
 
-        let fullimages = $(findImg).attr('title');
+        let fullimages = $(findImg);
 
         console.log(fullimages);
         for (let i = 0; i < fullimages.length; i++) {
