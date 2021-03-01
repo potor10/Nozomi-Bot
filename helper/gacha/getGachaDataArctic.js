fe17933e2d217b6bd85b3c031810209ff5d3dbd1
Potor <liupotor4@gmail.com>
2021-02-28 16:40:18 -0800

    new

--- helper/gacha/getGachaDataArctic.js
+++ helper/gacha/getGachaDataArctic.js
@@ -22,16 +22,16 @@ module.exports = async (client, href) => {
 
         console.log(skills[0]);
 
-        currentChar.ubskillname = $(skills[0]).text().trim().slice(0, -1);
+        currentChar.ubskillname = $('text', skills[0]).first().text().replaceAll('\n', '').trim();
         currentChar.ubskill = $('p', skills[0]).text().trim().slice(0, -1);
 
-        currentChar.skill1name = $(skills[1]).text().trim().slice(0, -1);
+        currentChar.skill1name = $(skills[1]).text().trim().replaceAll('\n', '').slice(0, -1);
         currentChar.skill1 = $('p', skills[1]).text().trim().slice(0, -1);
 
-        currentChar.skill2name = $(skills[2]).text().trim().slice(0, -1);
+        currentChar.skill2name = $(skills[2]).text().trim().replaceAll('\n', '').slice(0, -1);
         currentChar.skill2 = $('p', skills[2]).text().trim().slice(0, -1);
 
-        currentChar.exskill = $(skills[3]).text().trim().slice(0, -1) ;
+        currentChar.exskill = $(skills[3]).text().trim().replaceAll('\n', '').slice(0, -1) ;
         currentChar.exskill = $('p', skills[3]).text().trim().slice(0, -1);
 
         const stats1 = $('.splitsection > .lhalf');
