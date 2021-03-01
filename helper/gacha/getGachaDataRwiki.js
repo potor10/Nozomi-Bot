56c02c06a5f83e2fc7184dc792f36c41131dbc09
Potor <liupotor4@gmail.com>
2021-02-28 19:12:43 -0800

    new

--- helper/gacha/getGachaDataRwiki.js
+++ helper/gacha/getGachaDataRwiki.js
@@ -2,23 +2,21 @@ module.exports = async (client, href, charname, starlevel) => {
     const cheerio = require('cheerio');
     const got = require("got");
     
-    const findImg = '.ie5 > table > tbody > tr > td';
+    const findImg = '.ie5 > table > tbody > tr > td > .style_td img';
 
     try {
 		const response = await got(href);
         let innerPage = cheerio.load(response.body);
 
-        let fullimageurl = innerPage(findImg).has('.style_td img').filter(() => {
-            console.log(this.rowspan);
-            return this.rowspan == 11;
-          });
-        
-        //.attr('src');
+        let fullimages = innerPage(findImg);
 
-        console.log(fullimageurl);
-        // .style_td img
-        
-        client.gachaData[starlevel + 1][charname].fullimageurl = fullimageurl
+        for (let image in fullimages) {
+            console.log(fullimages[image]);
+            if (fullimages[image].attr('title').indexOf('icon') == -1) {
+                client.gachaData[starlevel + 1][charname].fullimageurl = fullimages[image].attr('src');
+                break;
+            }
+        }
     } catch (error) {
         console.log(error);
         //=> 'Internal server error ...'
