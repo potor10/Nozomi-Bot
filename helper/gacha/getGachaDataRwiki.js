8dcb0dcb2c82dc7b50204dffad35b1bf1e3f0479
Potor <liupotor4@gmail.com>
2021-02-28 19:44:09 -0800

    new

--- helper/gacha/getGachaDataRwiki.js
+++ helper/gacha/getGachaDataRwiki.js
@@ -12,9 +12,9 @@ module.exports = async (client, href, charname, jpcharname, starlevel) => {
         let titlestr = `キャラ/${'★'.repeat(starlevel)}/${jpcharname}`;
 
         for (let i = 0; i < fullimages.length; i++) {
-            if (fullimages[i].attr('title') == titlestr) {
+            if ($(fullimages[i]).attr('title') == titlestr) {
                 console.log("POGGER");
-                client.gachaData[starlevel + 1][charname].fullimageurl = fullimages[i].attr('src');
+                client.gachaData[starlevel + 1][charname].fullimageurl = $(fullimages[i]).attr('src');
                 break;
             }
         }
