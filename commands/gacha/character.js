0a03bdad8e9970b2dd55cb4cbd5abb349d2e92a4
Potor <liupotor4@gmail.com>
2021-02-28 21:08:42 -0800

    new

--- commands/gacha/character.js
+++ commands/gacha/character.js
@@ -38,14 +38,55 @@ module.exports = {
 
             if (starlevel != -1) {
                 let charFullImg = client.gachaData[starlevel][charname].fullimageurl;
-    
+                let charSubImg = client.gachaData[starlevel][charname].subimageurl;
+
+                let height = client.gachaData[starlevel][charname].height;
+                let birthday = client.gachaData[starlevel][charname].birthday;
+                let age = client.gachaData[starlevel][charname].age;
+                let species = client.gachaData[starlevel][charname].species;
+                let guild = client.gachaData[starlevel][charname].guild;
+                let likes = client.gachaData[starlevel][charname].likes;
+                let cv = client.gachaData[starlevel][charname].cv;
+                let realname = client.gachaData[starlevel][charname].realname;
+                let weight = client.gachaData[starlevel][charname].weight;
+                let bloodtype = client.gachaData[starlevel][charname].bloodtype;
+
+                let ubskillname = client.gachaData[starlevel][charname].ubskillname;
+                let ubskill = client.gachaData[starlevel][charname].ubskill;
+                let skill1name = client.gachaData[starlevel][charname].skill1name;
+                let skill1 = client.gachaData[starlevel][charname].skill1;
+                let skill2name = client.gachaData[starlevel][charname].skill2name;
+                let skill2 = client.gachaData[starlevel][charname].skill1;
+                let exskillname = client.gachaData[starlevel][charname].exskillname;
+                let exskill = client.gachaData[starlevel][charname].exskill;
+
                 let starstr = '★'.repeat(starlevel);
     
                 let messageDisplay = new MessageEmbed().setColor(`#${Math.floor(Math.random()*16777215).toString(16)}`)
                     .setAuthor(client.user.username, client.user.avatarURL())
                     .setTitle(`${starstr} ${charname.split(/,\s?/)[0]}`)
-                    .setDescription(`Owned By ${message.author.displayName||message.author.username}`)
+                    .setDescription(`${charname.split(/,\s?/)[1]}`)
+                    .setThumbnail(charSubImg)
                     .setImage(`${charFullImg}`)
+                    .addFields(
+                        { name: `Height`, value: height, inline: false },
+                        { name: `Birthday`, value: birthday, inline: false },
+                        { name: `Age`, value: age, inline: false },
+                        { name: `Species`, value: species, inline: false },
+                        { name: `Guild`, value: guild, inline: false },
+                        { name: `Likes`, value: likes, inline: false },
+                        { name: `CV`, value: cv, inline: false },
+                        { name: `Real Name`, value: realname, inline: false },
+                        { name: `Weight`, value: weight, inline: false },
+                        { name: `Blood Type`, value: bloodtype, inline: false },
+
+                        { name: '\u200b', value: '\u200b', inline: false },
+
+                        { name: `Union Burst: ${ubskillname}`, value: ubskill, inline: false },
+                        { name: `Skill 1: ${skill1name} `, value: skill1, inline: false },
+                        { name: `Skill 2: ${skill2name} `, value: skill2, inline: false },
+                        { name: `EX Skill: ${exskillname}`, value: exskill, inline: false }
+                    )
                     .setFooter(client.config.discord.footerText, client.user.avatarURL())
                     .setTimestamp();
                 
