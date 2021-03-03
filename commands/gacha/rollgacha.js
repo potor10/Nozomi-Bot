8c3e252226554401cd67a0c0becb841e659167f0
Potor <liupotor4@gmail.com>
2021-03-02 20:48:52 -0800

    new stuff

--- commands/gacha/rollgacha.js
+++ commands/gacha/rollgacha.js
@@ -20,7 +20,7 @@ module.exports = {
             .setAuthor(client.user.username, client.user.avatarURL())
             .setTitle(`Rolling x10 On This Gacha Will Cost **${jewelCost}** ${client.emotes.jewelEmoji}`)
             .setDescription(`React To Confirm`)
-            .attachFiles([`./img/entry_lucky.gif`, `./img/entry_unlucky.gif`])
+            .attachFiles([`./img/entry_lucky.gif`])
             .setFooter(client.config.discord.footerText, client.user.avatarURL())
             .setTimestamp();
         
